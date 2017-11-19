a0c6153a96fc28242e2635be971999a5db31a04d
deman2332 <dimas97@mail.ru>
2017-11-19 22:48:36 +0200

    add BV

--- src/ImageCompression/Utils/Objects/ByteVector.kt
+++ src/ImageCompression/Utils/Objects/ByteVector.kt
@@ -1,4 +1,169 @@
 package ImageCompression.Utils.Objects
 
-class ByteVector {
+import java.util.*
+import java.util.function.Consumer
+import kotlin.experimental.and
+import kotlin.experimental.or
+
+/**
+ * Non save class
+ * Iterator for Array<Byte>
+ */
+class ByteVector(size:Int=10):Iterable<Byte> {
+    companion object {
+        @JvmStatic val BITS_IN_BYTE=8
+    }
+    var bytes:Array<Byte>
+    var size:Int
+        private set;
+    var maxSize:Int
+        private set;
+    var curIndex:Int
+        private set;
+
+    init {
+        maxSize=size
+        if(size<=0)
+            maxSize=10
+
+        val zero:Byte = 0
+        bytes= Array(maxSize,{ x->zero})
+        this.size=0
+        curIndex=0
+    }
+
+    fun getByteArray():ByteArray{
+        return ByteArray(size,{x->bytes[x]})
+    }
+
+    fun append(byte: Byte){
+        if(size==maxSize-1){
+            grow(maxSize*2)
+        }
+        bytes[size++]=byte
+    }
+    fun append(short:Short){
+        append((short.toInt() shr BITS_IN_BYTE).toByte())
+        append(short.toByte())
+    }
+    fun append(long: Long){
+        append((long shr (BITS_IN_BYTE*7)).toByte())
+        append((long shr (BITS_IN_BYTE*6)).toByte())
+        append((long shr (BITS_IN_BYTE*5)).toByte())
+        append((long shr (BITS_IN_BYTE*4)).toByte())
+        append((long shr (BITS_IN_BYTE*3)).toByte())
+        append((long shr (BITS_IN_BYTE*2)).toByte())
+        append((long shr BITS_IN_BYTE).toByte())
+        append(long.toByte())
+    }
+
+    operator fun get(index: Int):Byte{
+        return bytes[index]
+    }
+    fun getByteAt(index:Int):Byte{
+        return bytes[index]
+    }
+    fun getNext():Byte{
+        return bytes[curIndex++]
+    }
+
+    fun getShortFrom(index: Int):Short{
+        var res=bytes[index].toInt()and 0xFF
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+1].toInt()and 0xFF)
+        return res.toShort()
+    }
+    fun getNextShort():Short{
+        var res=bytes[curIndex++].toInt()and 0xFF
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toInt()and 0xFF)
+        return res.toShort()
+    }
+
+    fun getLongFrom(index: Int):Long{
+        var res=(bytes[index].toLong() and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+1].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+2].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+3].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+4].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+5].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+6].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[index+7].toLong()and 0xFF)
+        return res
+    }
+    fun getNextLong():Long{
+        var res=(bytes[curIndex++].toLong() and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        res=(res shl BITS_IN_BYTE)
+        res = res or (bytes[curIndex++].toLong()and 0xFF)
+        return res
+    }
+
+    fun refreshIterator(){
+        curIndex=0
+    }
+
+    private fun grow(newSize:Int){
+        bytes=Array(newSize,{x->
+            if(x<size)
+                bytes[x]
+            else
+                0
+        })
+        maxSize=newSize
+    }
+
+    fun forEach(myAction:((byte:Byte)->Unit) ) {
+        for(i in 0..size-1)
+            myAction.invoke(bytes[i])
+    }
+    fun copy():ByteVector{
+        val res=ByteVector(size)
+        this.forEach(myAction= {byte->res.append(byte)})
+        return res
+    }
+
+    override fun iterator(): Iterator<Byte> {
+        class MyIterator:Iterator<Byte>{
+            val cur=0
+            override fun hasNext(): Boolean {
+                return cur<size
+            }
+
+            override fun next(): Byte {
+                return bytes[cur]
+            }
+        }
+        return MyIterator()
+    }
+    override fun equals(other: Any?): Boolean {
+        if(other is ByteVector){
+            if(this.size!=other.size)
+                return false
+            for(i in 0..size-1){
+                if(this[i]!=other[i])
+                    return false
+            }
+            return true
+        }else
+            return false
+    }
 }
--- src/ImageCompression/Utils/Objects/DataOPC.java
+++ src/ImageCompression/Utils/Objects/DataOPC.java
@@ -1,164 +1,174 @@
-package ImageCompression.Utils.Objects
+package ImageCompression.Utils.Objects;
 
-import java.math.BigInteger
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.util.Vector
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Queue;
+import java.util.Vector;
 
 /**
  * Created by Димка on 27.09.2016.
  */
-class DataOPC {
+public class DataOPC {
+    final static int SIZEOFBLOCK=8;
 
-    var base: ShortArray
-    var sign: Array<BooleanArray>
-    var DC: Short = 0
-    var N: BigInteger
-    var vectorCode: Vector<Long>
+    public short [] base;
+    public boolean [][] sign;
+    public short DC;
+    public BigInteger N;
+    public Vector<Long> Code;
 
-    val dc: ByteArray
-        get() = DC
-
-    init {
-        base = ShortArray(SIZEOFBLOCK)
-        sign = Array(SIZEOFBLOCK) { BooleanArray(SIZEOFBLOCK) }
-        N = BigInteger("0")
-        vectorCode = Vector()
+    public DataOPC() {
+        base=new short[SIZEOFBLOCK];
+        sign=new boolean[SIZEOFBLOCK][SIZEOFBLOCK];
+        N=new BigInteger("0");
+        Code=new Vector<Long>();
     }
 
 
-    fun FromBigIntToArray(): ByteArray {
-        return N.toByteArray()
+    public byte[] FromBigIntToArray() {
+        return N.toByteArray();
+    }
+    public void FromArrayToBigInt(byte[] code) {
+        N=new BigInteger(code);
     }
 
-    fun FromArrayToBigInt(code: ByteArray) {
-        N = BigInteger(code)
-    }
-
-    fun FromSignToArray(): ByteArray {
-        val res = ByteArray(SIZEOFBLOCK / 2)
-        for (i in 0 until SIZEOFBLOCK) {
-            for (j in 0 until SIZEOFBLOCK) {
-                res[i] = res[i] shl 1
-                if (sign[i][j]) {
-                    res[i] = res[i] or 1
+    public byte[] FromSignToArray() {
+        byte [] res=new byte[SIZEOFBLOCK/2];
+        for(int i=0;i<SIZEOFBLOCK;i++)
+        {
+            for(int j=0;j<SIZEOFBLOCK;j++)
+            {
+                res[i]<<=1;
+                if(sign[i][j]){
+                    res[i]|=1;
                 }
             }
         }
-
-        return res
+        return res;
     }
-
-    fun FromArrayToSing(s: ByteArray) {
-        for (i in SIZEOFBLOCK - 1 downTo 0) {
-            for (j in SIZEOFBLOCK - 1 downTo 0) {
-                sign[i][j] = s[i] and 1 == 1
-                s[i] = s[i] shr 1
+    public void FromArrayToSing(byte[] s) {
+        for(int i=SIZEOFBLOCK-1;i>=0;i--)
+        {
+            for(int j=SIZEOFBLOCK-1;j>=0;j--)
+            {
+                sign[i][j]=((s[i]&1)==1);
+                s[i]>>=1;
             }
         }
     }
 
-    fun FromBaseToArray(): ByteArray {
-        val ofset = SIZEOFBLOCK / 2
+    public byte[] FromBaseToArray() {
+        int ofset=SIZEOFBLOCK/2;
 
-        val res = ByteArray(SIZEOFBLOCK / 2)
-        for (i in res.indices) {
-            res[i] = base[i].toByte()
+        byte[] res=new byte[SIZEOFBLOCK/2];
+        for(int i=0;i<res.length;i++){
+            res[i]=(byte)base[i];
         }
-        return res
+        return res;
+    }
+    public void FromArrayToBase(byte[] b) {
+        for(int i=0;i<b.length;i++) {
+            base[i]=(short)((b[i])&0xff);
+        }
     }
 
-    fun FromArrayToBase(b: ByteArray) {
-        for (i in b.indices) {
-            base[i] = (b[i] and 0xff).toShort()
-        }
+    public byte[] getDC() {
+        byte[] res=new byte[2];
+        res[0]=(byte)(DC>>SIZEOFBLOCK);
+        res[1]=(byte)DC;
+        return res;
+    }
+    public void setDC(byte[] bytes) {
+        this.DC = bytes[0];
+        DC<<=SIZEOFBLOCK;
+        DC|=bytes[1];
     }
 
-    fun setDC(DC: Short) {
-        this.DC = DC
+    public Vector<Long> getVectorCode(){
+        return Code;
+    }
+    public void setVectorCode(Vector<Long> v)
+    {
+        Code=v;
     }
 
-    fun toString(flag: Flag): String {
-        val offset = SIZEOFBLOCK / 2
-        val sb = StringBuilder()
+    public String toString(Flag flag) {
+        int offset=SIZEOFBLOCK/2;
+        StringBuilder sb=new StringBuilder();
 
-        if (flag.isOneFile && !flag.isGlobalBase)
-            for (b in FromBaseToArray())
-                sb.append(b.toChar())
+        if(flag.isOneFile()&&!flag.isGlobalBase())
+            for(short b : FromBaseToArray())
+                sb.append((char)b);
 
-        val sign = FromSignToArray()
-        for (i in sign.indices) {
-            sb.append(sign[i].toChar())
+        byte[] sign=FromSignToArray();
+        for(int i=0;i<sign.length;i++){
+            sb.append((char)sign[i]);
         }
 
-        if (flag.isDC)
-            sb.append(DC.toChar())
+        if(flag.isDC())
+            sb.append((char)DC);
 
-        if (!flag.isLongCode) {
-            val n = FromBigIntToArray()
-            sb.append(n.size.toChar())
-            for (c in n) {
-                sb.append(c.toShort().toInt())
+        if(!flag.isLongCode()){
+            byte[] n =FromBigIntToArray();
+            sb.append((char)n.length);
+            for(short c : n){
+                sb.append((short)c);
             }
-        } else {
-            sb.append(vectorCode.size.toChar())
-            for (i in vectorCode.indices) {
-                var v = vectorCode.elementAt(i)
-                sb.append(v.toChar())
-                v = v shr 16
-                sb.append(v.toChar())
-                v = v shr 16
-                sb.append(v.toChar())
-                v = v shr 16
-                sb.append(v.toChar())
-            }
-        }
-
-        return sb.toString()
-    }
-
-    fun valueOf(s: String, flag: Flag) {
-        val offset = SIZEOFBLOCK / 2
-        var index = 0
-
-        if (flag.isOneFile && !flag.isGlobalBase) {
-            val a = ShortArray(offset)
-            for (i in 0 until offset) {
-                a[i] = s[index++].toShort()
-            }
-            FromArrayToBase(a)
-        }
-
-        val sign = ShortArray(offset)
-        for (i in 0 until offset) {
-            sign[i] = s[index++].toShort()
-        }
-        FromArrayToSing(sign)
-
-        if (flag.isDC)
-            DC = s[index++].toShort()
-
-        if (!flag.isLongCode) {
-            val length = s[index++].toInt()
-            val code = ShortArray(length)
-            for (i in 0 until length) {
-                code[i] = s[index++].toShort()
-            }
-            FromArrayToBigInt(code)
-        } else {
-            val length = s[index++].toInt()
-            for (i in 0 until length) {
-                var v = s[index++].toLong()
-                v = v or (s[index++].toLong() shl 16)
-                v = v or (s[index++].toLong() shl 32)
-                v = v or (s[index++].toLong() shl 48)
-                vectorCode.add(v)
+        }else{
+            sb.append((char)Code.size());
+            for(int i=0;i<Code.size();i++){
+                long v=Code.elementAt(i);
+                sb.append((char)v);
+                v>>=16;
+                sb.append((char)v);
+                v>>=16;
+                sb.append((char)v);
+                v>>=16;
+                sb.append((char)v);
             }
         }
 
+        return sb.toString();
     }
+    public void valueOf(String s,Flag flag){
+        int offset=SIZEOFBLOCK;
+        int index=0;
 
-    companion object {
-        internal val SIZEOFBLOCK = 8
+        if(flag.isOneFile()&&!flag.isGlobalBase()) {
+            byte[] a=new byte[offset];
+            for (int i = 0; i < offset; i++) {
+                a[i] = (byte) s.charAt(index++);
+            }
+            FromArrayToBase(a);
+        }
+
+        byte[] sign=new byte[offset];
+        for(int i=0;i<offset;i++){
+            sign[i]=(byte) s.charAt(index++);
+        }
+        FromArrayToSing(sign);
+
+        if(flag.isDC())
+            DC=(short)s.charAt(index++);
+
+        if(!flag.isLongCode()){
+            int length=s.charAt(index++);
+            byte[] code=new byte[length];
+            for(int i=0;i<length;i++){
+                code[i]=(byte) s.charAt(index++);
+            }
+            FromArrayToBigInt(code);
+        }else{
+            int length=s.charAt(index++);
+            for(int i=0;i<length;i++){
+                long v=(long)s.charAt(index++);
+                v|=(long)(s.charAt(index++))<<16;
+                v|=(long)(s.charAt(index++))<<32;
+                v|=(long)(s.charAt(index++))<<48;
+                Code.add(v);
+            }
+        }
+
     }
 }
