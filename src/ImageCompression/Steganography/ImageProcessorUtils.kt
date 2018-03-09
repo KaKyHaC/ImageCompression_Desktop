7527082aea1e8deed2a6ea6c8e602440fcf1cd99
deman2332 <dimas97@mail.ru>
2018-03-09 16:18:56 +0200

    ImageProcessorUtils

--- src/ImageCompression/Steganography/ImageProcessorUtils.kt
+++ src/ImageCompression/Steganography/ImageProcessorUtils.kt
@@ -7,57 +7,62 @@ import ImageCompression.Steganography.Containers.UnitContainer
 import java.awt.Color
 import java.awt.image.BufferedImage
 
-class ImageProcessor (val width:Int,val height:Int){
+class ImageProcessorUtils {
     data class Triple<T>(var r:T?,var g:T?,var b:T?)
-
-    var units=Triple<IContainer<UnitContainer<Short>>>(null,null,null)
-    var opcs=Triple<IContainer<OpcContainer<Short>>>(null,null,null)
-    var image:BufferedImage
-
-    constructor(image:BufferedImage):this(image.width,image.height){
-        this.image=image
-    }
-    init{
-        this.image= BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR)
-    }
-    fun imageToUnits(unit_W: Int,unit_H: Int){
+////    var image:BufferedImage
+////
+//    constructor(image:BufferedImage):this(image.width,image.height){
+//        this.image=image
+//    }
+//    init{
+//        this.image= BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR)
+//    }
+    fun imageToUnits(image:BufferedImage,unit_W: Int,unit_H: Int): Triple<IContainer<UnitContainer<Short>>> {
         val r=Container<Short>(image.width,image.height){i,j->Color(image.getRGB(i,j)).red.toShort()}
         val g=Container<Short>(image.width,image.height){i,j->Color(image.getRGB(i,j)).green.toShort()}
         val b=Container<Short>(image.width,image.height){i,j->Color(image.getRGB(i,j)).blue.toShort()}
 
+        val units=Triple<IContainer<UnitContainer<Short>>>(null,null,null)
+
         units.r=UnitContainerFactory.getContainers(r,unit_W,unit_H)
         units.g=UnitContainerFactory.getContainers(g,unit_W,unit_H)
         units.b=UnitContainerFactory.getContainers(b,unit_W,unit_H)
+
+        return units
     }
-    fun directStego():Triple<IContainer<OpcContainer<Short>>>{
+    fun directStego(units:Triple<IContainer<UnitContainer<Short>>>):Triple<IContainer<OpcContainer<Short>>>{
+        val opcs=Triple<IContainer<OpcContainer<Short>>>(null,null,null)
+
         opcs.r=directConvert(units.r!!)
         opcs.g=directConvert(units.g!!)
         opcs.b=directConvert(units.b!!)
         return opcs
     }
-    fun reverceStego(unit_W: Int,unit_H: Int,isMultiTWO: Boolean):Triple<IContainer<UnitContainer<Short>>>{
+    fun reverceStego(opcs:Triple<IContainer<OpcContainer<Short>>>, unit_W: Int,unit_H: Int,isMultiTWO: Boolean):Triple<IContainer<UnitContainer<Short>>>{
+        val units=Triple<IContainer<UnitContainer<Short>>>(null,null,null)
+
         units.r=reverceConvert(opcs.r!!,unit_W, unit_H, isMultiTWO)
         units.g=reverceConvert(opcs.g!!,unit_W, unit_H, isMultiTWO)
         units.b=reverceConvert(opcs.b!!,unit_W, unit_H, isMultiTWO)
         return units
     }
 
-    fun getMessageMaxSize():Int{
+    fun getMessageMaxSize(units:Triple<IContainer<UnitContainer<Short>>>):Int{
         var len=0
-        units.r?.let { len+=width*height }
-        units.g?.let { len+=width*height }
-        units.b?.let { len+=width*height }
+        units.r?.let { len+=units.r!!.width*units.r!!.height }
+        units.g?.let { len+=units.g!!.width*units.g!!.height }
+        units.b?.let { len+=units.b!!.width*units.b!!.height }
         return len
     }
 
-    fun setMessage(message: BooleanArray):Int{
+    fun setMessage(units:Triple<IContainer<UnitContainer<Short>>>,message: BooleanArray):Int{
         var index=units.r?.setMesasge(message,0)?:0
         index=units.r?.setMesasge(message,index)?:index
         index=units.r?.setMesasge(message,index)?:index
         return index
     }
-    fun getMessage():BooleanArray{
-        val message=BooleanArray(getMessageMaxSize())
+    fun getMessage(units:Triple<IContainer<UnitContainer<Short>>>):BooleanArray{
+        val message=BooleanArray(getMessageMaxSize(units))
         var index=units.r?.setMesasge(message,0)?:0
         index=units.r?.setMesasge(message,index)?:index
         index=units.r?.setMesasge(message,index)?:index
