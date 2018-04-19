2c33d62d70eafa110782412e5369c4d3d83a5516
deman2332 <dimas97@mail.ru>
2018-04-19 23:03:22 +0300

    from Java 9 to 8

--- ImageCompressionLib/src/ImageCompressionLib/Constants/Constants.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Constants/Constants.kt
@@ -1,5 +1,8 @@
 package ImageCompressionLib.Constants
 
+import java.math.BigInteger
+
 val SIZEOFBLOCK=8;
+val TWO=BigInteger.valueOf(2)
 var MAX_LONG= Math.pow(2.0, 54.0).toLong()
 var BINARY_VALUE_OF_POSITION=1
