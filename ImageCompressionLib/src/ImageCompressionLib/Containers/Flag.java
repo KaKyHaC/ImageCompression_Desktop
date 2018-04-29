16ec31ef1c9c8270f0e42d26b9cb428c401ab45e
deman2332 <dimas97@mail.ru>
2018-04-29 12:23:46 +0300

    upgrade Flag

--- ImageCompressionLib/src/ImageCompressionLib/Containers/Flag.java
+++ ImageCompressionLib/src/ImageCompressionLib/Containers/Flag.java
@@ -7,16 +7,19 @@ import ImageCompressionLib.Utils.Functions.Steganography;
  */
 
 public class Flag {
-    public enum Parameter{
-        OneFile(16),Enlargement(32),DC(64),LongCode(128),GlobalBase(256),
-        Password(512),Steganography(1024),Alignment(2048),CompressionUtils(4096);
 
+    public enum Parameter{
+        // empty value 8
+        OneFile(16),Enlargement(32),DC(64),LongCode(128),GlobalBase(256),
+        Password(512),Steganography(1024),Alignment(2048),CompressionUtils(4096),Quantization(1),Encryption(4),DCT(2);
         final int value;
         Parameter(int i) {
             value=i;
         }
     }
+    @Deprecated
     public enum QuantizationState {Without, First}
+    @Deprecated
     public enum Encryption {Without, First}
 
     private short f;
@@ -28,9 +31,21 @@ public class Flag {
     public Flag(short val){
         f=val;
     }
+    public static Flag createDefaultFlag(){
+        Flag f=new Flag();
+        f.setTrue(Parameter.OneFile);
+        f.setTrue(Parameter.Enlargement);
+        f.setTrue(Parameter.DC);
+        f.setTrue(Parameter.LongCode);
+        f.setTrue(Parameter.CompressionUtils);
+        f.setTrue(Parameter.Quantization);
+        f.setTrue(Parameter.DCT);
+        return f;
+    }
 
     public short getFlag(){return f;}
 
+    @Deprecated
     public QuantizationState getQuantization(){
         byte q=(byte)(f&3);
         if(q==0)
@@ -40,6 +55,7 @@ public class Flag {
         else
             return null;
     }
+    @Deprecated
     public void setQuantization(QuantizationState qs){
         switch (qs) {
             case Without:f&=(~3);
@@ -49,6 +65,7 @@ public class Flag {
         }
     }
 
+    @Deprecated
     public Encryption getEncryption(){
         byte e=(byte)(f&12);
         switch (e){
@@ -57,6 +74,7 @@ public class Flag {
             default: return null;
         }
     }
+    @Deprecated
     public void setEncryption(Encryption e){
         switch (e) {
             case Without: f&=(~12);
@@ -87,6 +105,12 @@ public class Flag {
         } else {
             f&=(~parameter.value);
         }
+    }
+    public void setTrue(Parameter parameter){
+        setChecked(parameter,true);
+    }
+    public void setFalse(Parameter parameter){
+        setChecked(parameter,false);
     }
 
     @Override
