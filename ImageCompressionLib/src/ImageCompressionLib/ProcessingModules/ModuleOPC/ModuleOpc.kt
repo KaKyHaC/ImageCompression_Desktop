f661570af029ee84eecd238d735bf0ec935f26c3
deman2332 <dimas97@mail.ru>
2018-05-04 20:33:29 +0300

    ModuleOpc

--- ImageCompressionLib/src/ImageCompressionLib/ProcessingModules/ModuleOPC/ModuleOpc.kt
+++ ImageCompressionLib/src/ImageCompressionLib/ProcessingModules/ModuleOPC/ModuleOpc.kt
@@ -1,5 +1,6 @@
 package ImageCompressionLib.ProcessingModules.ModuleOPC
 
+import ImageCompressionLib.Constants.DEFAULT_PASSWORD
 import ImageCompressionLib.Containers.*
 import ImageCompressionLib.Containers.Matrix.DataOpcMatrix
 import ImageCompressionLib.Constants.State
@@ -7,10 +8,11 @@ import ImageCompressionLib.Containers.Matrix.Matrix
 import ImageCompressionLib.Containers.Matrix.ShortMatrix
 import ImageCompressionLib.Containers.Type.ByteVector
 import ImageCompressionLib.Containers.Type.DataOpc
+import ImageCompressionLib.Containers.Type.Flag
+import ImageCompressionLib.Utils.Functions.Encryption
 import ImageCompressionLib.Utils.Objects.OpcConvertor
 import java.util.ArrayList
 import java.util.concurrent.Callable
-import java.util.concurrent.ExecutionException
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
@@ -21,33 +23,38 @@ class ModuleOpc {
     val parameters: Parameters
     var isAsyn: Boolean = false
 
+    var tripleShortMatrix:TripleShortMatrix?=null
+    var tripleDataOpcMatrix:TripleDataOpcMatrix?=null
+
     constructor(tripleShortMatrix: TripleShortMatrix, parameters: Parameters, isAsyn: Boolean = true) {
         this.isAsyn = isAsyn
         this.parameters = parameters
+        this.tripleShortMatrix=tripleShortMatrix
 
         a = OpcConvertor(ShortMatrix.valueOf(tripleShortMatrix.a), parameters)
         b = OpcConvertor(ShortMatrix.valueOf(tripleShortMatrix.b), parameters)
         c = OpcConvertor(ShortMatrix.valueOf(tripleShortMatrix.c), parameters)
     }
 
-    constructor(tripleDataOpcOld: TripleDataOpcMatrix, parameters: Parameters, isAsyn: Boolean = true) {
+    constructor(tripleDataOpcMatrix: TripleDataOpcMatrix, parameters: Parameters, isAsyn: Boolean = true) {
         this.isAsyn = isAsyn
         this.parameters = parameters
+        this.tripleDataOpcMatrix= tripleDataOpcMatrix
 
-        a = OpcConvertor(DataOpcMatrix.valueOf(tripleDataOpcOld.a), parameters)
-        b = OpcConvertor(DataOpcMatrix.valueOf(tripleDataOpcOld.b), parameters)
-        c = OpcConvertor(DataOpcMatrix.valueOf(tripleDataOpcOld.c), parameters)
+        a = OpcConvertor(DataOpcMatrix.valueOf(tripleDataOpcMatrix.a), parameters)
+        b = OpcConvertor(DataOpcMatrix.valueOf(tripleDataOpcMatrix.b), parameters)
+        c = OpcConvertor(DataOpcMatrix.valueOf(tripleDataOpcMatrix.c), parameters)
     }
 
 
-    fun directOPCMultiThreads(encryptParameters: EncryptParameters): TripleDataOpcMatrix { //multy thred
+    private fun directOPCMultiThreads(encryptParameters: EncryptParameters?=null): TripleDataOpcMatrix { //multy thred
         val executorService = Executors.newFixedThreadPool(3)
         val futures = ArrayList<Future<Matrix<DataOpc>>>()
 
         appendTimeManager("direct OPC")
-        futures.add(executorService.submit(Callable { a.getDataOpcs(encryptParameters.stegoPosition, encryptParameters.message) }))
-        futures.add(executorService.submit(Callable { b.getDataOpcs(encryptParameters.stegoPosition, encryptParameters.message) }))
-        futures.add(executorService.submit(Callable { c.getDataOpcs(encryptParameters.stegoPosition, encryptParameters.message) }))
+        futures.add(executorService.submit(Callable { a.getDataOpcs(encryptParameters?.stegoPosition, encryptParameters?.message) }))
+        futures.add(executorService.submit(Callable { b.getDataOpcs(encryptParameters?.stegoPosition, encryptParameters?.message) }))
+        futures.add(executorService.submit(Callable { c.getDataOpcs(encryptParameters?.stegoPosition, encryptParameters?.message) }))
 
 
         val b = (futures[1].get())
@@ -62,15 +69,15 @@ class ModuleOpc {
 
     }
 
-    fun reverseOPCMultiThreads(encryptParameters: EncryptParameters): Pair<TripleShortMatrix, ByteVector?> {
+    private fun reverseOPCMultiThreads(encryptParameters: EncryptParameters?=null): Pair<TripleShortMatrix, ByteVector?> {
         val executorService = Executors.newFixedThreadPool(3)
         val futures = ArrayList<Future<Pair<Matrix<Short>, ByteVector?>>>()
 
         appendTimeManager("start reOPC")
 
-        futures.add(executorService.submit(Callable { a.getDataOrigin(encryptParameters.stegoPosition) }))
-        futures.add(executorService.submit(Callable { b.getDataOrigin(encryptParameters.stegoPosition) }))
-        futures.add(executorService.submit(Callable { c.getDataOrigin(encryptParameters.stegoPosition) }))
+        futures.add(executorService.submit(Callable { a.getDataOrigin(encryptParameters?.stegoPosition) }))
+        futures.add(executorService.submit(Callable { b.getDataOrigin(encryptParameters?.stegoPosition) }))
+        futures.add(executorService.submit(Callable { c.getDataOrigin(encryptParameters?.stegoPosition) }))
 
         val (a1, v1) = futures[0].get()
         val (b1, v2) = futures[1].get()
@@ -81,5 +88,31 @@ class ModuleOpc {
     }
 
     private fun appendTimeManager(s: String) {}
+
+    private fun encode(encryptParameters: EncryptParameters){
+        if(parameters.flag.isChecked(Flag.Parameter.Encryption)) {
+            Encryption.encode(tripleDataOpcMatrix,encryptParameters.password ?: DEFAULT_PASSWORD)
+        }
+    }
+
+    fun getTripleDataOpcMatrix(encryptParameters: EncryptParameters?): TripleDataOpcMatrix {
+        if(tripleDataOpcMatrix==null) {
+            tripleDataOpcMatrix = directOPCMultiThreads(encryptParameters)
+            encryptParameters?.let { encode(encryptParameters) }
+        }
+        return tripleDataOpcMatrix!!
+    }
+
+    var m:ByteVector?=null
+        private set
+    fun getTripleShortMatrix(encryptParameters: EncryptParameters?): Pair<TripleShortMatrix, ByteVector?> {
+        if(tripleShortMatrix==null) {
+            encryptParameters?.let { encode(encryptParameters) }
+            val (tripleShortMatrix, mes) = reverseOPCMultiThreads(encryptParameters)
+            this.tripleShortMatrix = tripleShortMatrix
+            m=mes
+        }
+        return Pair(tripleShortMatrix!!,m)
+    }
 }
 
