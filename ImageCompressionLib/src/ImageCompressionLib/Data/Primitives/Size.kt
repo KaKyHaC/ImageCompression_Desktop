211d0ef2beb3843e19cf609e2296f665f86fd5ef
Dima <d.v.barannik@gamil.com>
2019-11-24 16:43:37 +0200

    Size.kt: implement ISaveable

--- ImageCompressionLib/src/ImageCompressionLib/Data/Primitives/Size.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Data/Primitives/Size.kt
@@ -1,18 +1,16 @@
 package ImageCompressionLib.Data.Primitives
 
-import ImageCompressionLib.Data.Type.ByteVector
+import ImageCompressionLib.Data.Interfaces.IByteVector
+import ImageCompressionLib.Data.Interfaces.ISaveable
 
-data class Size(val width: Int, val height: Int){
-    fun toByteVector(vector: ByteVector){
+data class Size(val width: Int, val height: Int) : ISaveable{
+
+    constructor(vector: IByteVector) : this(
+        vector.getNextShort()?.toInt() ?: 0,
+        vector.getNextShort()?.toInt() ?: 0
+    )
+    override fun appendByteVector(vector: IByteVector) {
         vector.append(width.toShort())
         vector.append(height.toShort())
-    }
-    companion object {
-        @JvmStatic fun valueOf(vector: ByteVector): Size {
-            return Size(
-                vector.getNextShort().toInt(),
-                vector.getNextShort().toInt()
-            )
-        }
     }
 }
