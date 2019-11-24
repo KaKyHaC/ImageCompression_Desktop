77dcae2edd454c1f24f4c24bf2f5c55fe5698245
Dima <d.v.barannik@gamil.com>
2019-11-24 20:27:18 +0200

    implement ImageDeviationUtils.kt

--- ImageCompressionLib/src/ImageCompressionLib/Data/Interfaces/IMatrix.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Data/Interfaces/IMatrix.kt
@@ -3,7 +3,8 @@ package ImageCompressionLib.Data.Interfaces
 interface IMatrix<T> {
     operator fun get(i: Int, j: Int): T?
     operator fun set(i: Int, j: Int, value: T)
-    fun forEachIndexed(invoke: (i: Int, j: Int, value: T) -> T?)
+    fun forEachIndexed(invoke: (i: Int, j: Int, value: T) -> Unit)
+    fun <R>mapIndexed(invoke: (i: Int, j: Int, value: T) -> R)
     val width: Int
     val height: Int
 }
--- ImageCompressionLib/src/ImageCompressionLib/Utils/Objects/ImageDeviationUtils.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Utils/Objects/ImageDeviationUtils.kt
@@ -1,12 +1,16 @@
 package ImageCompressionLib.Utils.Objects
 
-import ImageCompressionLib.Data.Interfaces.IDeviationable
 import ImageCompressionLib.Data.Interfaces.IMatrix
 import ImageCompressionLib.Data.Primitives.Pixel
 
 object ImageDeviationUtils {
 
-    fun calculateDeviation(other: IMatrix<IDeviationable<Pixel>>): Double {
-
+    fun calculateDeviation(first: IMatrix<Pixel>, second: IMatrix<Pixel>): Double {
+        var sum = 0.0
+        first.forEachIndexed{ i, j, value ->
+            sum += second[i, j]?.let { value.calculateDeviation(it) } ?: 0.0
+        }
+        sum /= first.width * first.height
+        return Math.sqrt(sum)
     }
 }
