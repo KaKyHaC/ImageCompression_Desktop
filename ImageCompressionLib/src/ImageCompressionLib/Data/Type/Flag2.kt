1b7b02a6e5770072a93c739f102873c15748016c
Dima <d.v.barannik@gamil.com>
2019-11-24 16:19:34 +0200

    improve Flag2.kt

--- ImageCompressionLib/src/ImageCompressionLib/Data/Type/Flag2.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Data/Type/Flag2.kt
@@ -1,42 +1,31 @@
 package ImageCompressionLib.Data.Type
 
-class Flag2 {
-    //TODO remove steganography
-    enum class Parameter  constructor(internal val value: Int) {
-        // empty value 8
-        OneFile(16),
-        Enlargement(32), DC(64), LongCode(128), GlobalBase(256),
-        Password(512), Steganography(1024), Alignment(2048),
-        CompressionUtils(4096), Quantization(1), Encryption(4), DCT(2)
-    }
-    val data:MutableMap<Parameter,Boolean>
+class Flag2 constructor() {
+    val data = mutableMapOf<Parameter, Boolean>()
 
-    constructor(){
-        data=HashMap<Parameter,Boolean>()
-        for(p in Parameter.values())
-            data.put(p,false)
+    constructor(vector: ByteVector) : this() {
+        for (p in Parameter.values())
+            data.put(p, vector.getNextBoolean())
     }
-    constructor(vector: ByteVector){
-        data=HashMap<Parameter,Boolean>()
-        for(p in Parameter.values())
-            data.put(p,vector.getNextBoolean())
-    }
-    fun toByteVector(vector: ByteVector){
-        for(p in Parameter.values())
+
+    fun toByteVector(vector: ByteVector) {
+        for (p in Parameter.values())
             vector.append(data.get(p)!!)
     }
 
-    fun isChecked(param: Parameter): Boolean {
-        return data.get(param)!!
-    }
-    fun setChecked(parameter: Parameter, state: Boolean) {
-        data.set(parameter,state)
-    }
-    fun setTrue(parameter: Parameter) {
-        setChecked(parameter, true)
-    }
-    fun setFalse(parameter: Parameter) {
-        setChecked(parameter, false)
+    fun isChecked(param: Parameter) = data.get(param) ?: false
+
+    fun setChecked(parameter: Parameter, state: Boolean) = data.set(parameter, state)
+
+    fun setTrue(parameter: Parameter) = setChecked(parameter, true)
+
+    fun setFalse(parameter: Parameter) = setChecked(parameter, false)
+
+    enum class Parameter {
+        OneFile, Enlargement, DC, LongCode, GlobalBase, Password, Steganography, Alignment, CompressionUtils, Quantization, Encryption, DCT;
+
+        fun getBinaryValue() = Math.pow(2.0, values().indexOf(this).toDouble())
+
     }
 
 
