c485668c4fe150c4ca7ab0c31059e253ae0cdb17
Dima <d.v.barannik@gamil.com>
2019-11-24 18:46:22 +0200

    IMatrix.kt: improved

--- ImageCompressionLib/src/ImageCompressionLib/Data/Interfaces/IMatrix.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Data/Interfaces/IMatrix.kt
@@ -1,7 +1,7 @@
 package ImageCompressionLib.Data.Interfaces
 
 interface IMatrix<T> {
-    operator fun get(i: Int, j: Int): T
+    operator fun get(i: Int, j: Int): T?
     operator fun set(i: Int, j: Int, value: T)
     fun forEachIndexed(invoke: (i: Int, j: Int, value: T) -> T?)
     val width: Int
