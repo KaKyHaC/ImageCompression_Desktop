24b5cbdd8cbdb6d8b0c63b578bab58898eaf2284
Nikita <nutsdev777@gmail.com>
2019-11-26 15:14:14 +0200

    MatrixUtils.kt: multiply Short

--- ImageCompressionLib/src/ImageCompressionLib/Utils/Objects/MatrixUtils.kt
+++ ImageCompressionLib/src/ImageCompressionLib/Utils/Objects/MatrixUtils.kt
@@ -5,15 +5,16 @@ import ImageCompressionLib.Data.Primitives.Size
 
 object MatrixUtils {
 
-    fun <T : Number> multiply(a: Matrix<T>, b: Matrix<T>): Matrix<T> {
-        val res = Matrix(Size(a.width, b.height)) { _, _ -> 0 }
+    fun multiply(a: Matrix<Short>, b: Matrix<Short>): Matrix<Short> {
+        val res = Matrix(Size(a.width, b.height)) { _, _ -> 0.toShort() }
         for (i in 0 until a.height) {
             for (j in 0 until b.width) {
                 for (k in 0 until a.width) {
-                    res[i,j] = a[i,k] * b [k,j]
+                    res[i,j] = (a[i,k]!! * b[k,j]!!).toShort()
                 }
             }
         }
         return res
     }
+
 }
