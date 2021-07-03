51db4867948e90cb0e233aa128f6d692cb116c16
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-03 15:24:08 +0300

    create DataOpc.kt

---
+++ ImageProcessingLib2021/src/data_model/containers/types/DataOpc.kt
@@ -0,0 +1,27 @@
+package data_model.containers.types
+
+import java.math.BigInteger
+import java.util.*
+
+sealed class DataOpc(
+        val base: ShortArray,
+        val sign: Array<BooleanArray>,
+        val DC: Short,
+        val size: Size
+) {
+    class BI(
+            base: ShortArray,
+            sign: Array<BooleanArray>,
+            DC: Short,
+            val N: BigInteger,
+            size: Size
+    ) : DataOpc(base, sign, DC, size)
+
+    class Long(
+            base: ShortArray,
+            sign: Array<BooleanArray>,
+            DC: Short,
+            val vectorCode: Vector<Long>,
+            size: Size
+    ) : DataOpc(base, sign, DC, size)
+}
