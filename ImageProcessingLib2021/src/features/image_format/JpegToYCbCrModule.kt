b7212a863bbb12e25c624ee37e4d8850f66efa89
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-03 19:56:13 +0300

    JpegToYCbCrModule.kt: conver any images

--- ImageProcessingLib2021/src/features/image_format/JpegToYCbCrModule.kt
+++ ImageProcessingLib2021/src/features/image_format/JpegToYCbCrModule.kt
@@ -3,15 +3,15 @@ package features.image_format
 import data_model.processing_data.ProcessingData
 import features.AbsDataProcessor
 
-class JpegToYCbCrModule() : AbsDataProcessor<ProcessingData.Image.Jpeg, ProcessingData.Image.YCbCr>(
-        ProcessingData.Image.Jpeg::class, ProcessingData.Image.YCbCr::class
+class JpegToYCbCrModule() : AbsDataProcessor<ProcessingData.Image, ProcessingData.Image>(
+        ProcessingData.Image::class, ProcessingData.Image::class
 ) {
 
-    override fun processDirectTyped(data: ProcessingData.Image.Jpeg): ProcessingData.Image.YCbCr {
+    override fun processDirectTyped(data: ProcessingData.Image): ProcessingData.Image {
         TODO("Not yet implemented")
     }
 
-    override fun processReverseTyped(data: ProcessingData.Image.YCbCr): ProcessingData.Image.Jpeg {
+    override fun processReverseTyped(data: ProcessingData.Image): ProcessingData.Image {
         TODO("Not yet implemented")
     }
 }
