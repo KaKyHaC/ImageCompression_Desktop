af1533356c758f6c3308010a7849717b34ef25c7
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-04 22:39:08 +0300

    create OpcProcessingUnitTest.kt

--- Tests_tmp/features/opc/utils/OpcProcessUtilsTest.kt
+++ Tests_tmp/features/opc/utils/OpcProcessUtilsTest.kt
@@ -1,16 +1,13 @@
-package ImageProcessingLib2021.features.opc
+package features.opc.utils
 
 import data_model.generics.matrix.Matrix
 import data_model.types.Size
-import features.opc.utils.OpcProcessUtils
 import org.junit.Test
 import java.util.*
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
-
-class OpcProcessUtilsTest {
-
+internal class OpcProcessUtilsTest {
     val random = Random()
 
     @Test
