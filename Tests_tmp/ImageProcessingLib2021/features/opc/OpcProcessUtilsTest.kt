d86a602b34f2c415c909bdfd91cafafa51f6bba6
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-04 22:32:36 +0300

    create OpcProcessUtilsTest.kt

--- Tests_tmp/ImageProcessingLib2021/features/opc/OpcProcessUtilsTest.kt
+++ Tests_tmp/ImageProcessingLib2021/features/opc/OpcProcessUtilsTest.kt
@@ -6,6 +6,7 @@ import features.opc.utils.OpcProcessUtils
 import org.junit.Test
 import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 
 class OpcProcessUtilsTest {
@@ -13,13 +14,25 @@ class OpcProcessUtilsTest {
     val random = Random()
 
     @Test
-    fun directProcess() {
-        directProcess(Size(8,8))
+    fun directProcess8x8() {
+        for (i in 4..16)
+            for (j in 4..16)
+                directProcess(Size(i, j))
+    }
+
+    @Test
+    fun directProcess50x50() {
+        directProcess(Size(50, 50))
+    }
+
+    @Test
+    fun directProcess100x100() {
+        directProcess(Size(100, 100))
     }
 
     fun directProcess(size: Size) {
         val preOpcParams = OpcProcessUtils.PreOpcParams()
-        val dataOrigin = Matrix.create(size) { i, j -> (random.nextInt()% 256).toShort()  }
+        val dataOrigin = Matrix.create(size) { i, j -> (random.nextInt() % 256).toShort() }
         val dataCopy = Matrix.create(dataOrigin.size) { i, j -> dataOrigin[i, j] }
         println("dataOrigin = $dataOrigin")
         val dataOpc = OpcProcessUtils.directProcess(preOpcParams, OpcProcessUtils.OpcParams(), dataOrigin)
@@ -27,5 +40,7 @@ class OpcProcessUtilsTest {
         val reverseProcess = OpcProcessUtils.reverseProcess(preOpcParams, dataOpc, size)
         println("reverseProcess = $reverseProcess")
         assertEquals(reverseProcess, dataCopy)
+        dataCopy[0, 0]++
+        assertNotEquals(reverseProcess, dataCopy)
     }
 }
