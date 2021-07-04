dadf1c9aad525fdc476b5bf85b0a12ffcf66efd5
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-04 22:54:48 +0300

    create MatrixUtilsTest.kt

---
+++ Tests_tmp/utils/MatrixUtilsTest.kt
@@ -0,0 +1,43 @@
+package utils
+
+import data_model.generics.matrix.Matrix
+import data_model.types.Size
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+
+internal class MatrixUtilsTest {
+
+    var matrix: Matrix<Int>? = null
+
+    @Before
+    fun setUp() {
+        matrix = Matrix.create(Size(10, 10)) { i, j -> i + j * i }
+    }
+
+    @Test
+    fun rectIterator1() {
+        rectIterator(1, 1, Size(2, 2), null)
+    }
+
+    @Test
+    fun rectIterator2() {
+        rectIterator(2, 2, Size(2, 2), null)
+    }
+
+    fun rectIterator(w: Int, h: Int, size: Size, default: Int?) {
+        matrix?.let {
+            val rectIterator = MatrixUtils.rectIterator(it, w, h, size, default)
+            assertEquals(it[w, h], rectIterator[0, 0])
+            assertEquals(it[w + size.width - 1, h + size.height - 1], rectIterator[size.width - 1, size.height - 1])
+            assertNotEquals(it[w + size.width - 1, h - 1], rectIterator[size.width - 1, size.height - 1])
+            assertNotEquals(it[w - 1, h + size.height - 1], rectIterator[size.width - 1, size.height - 1])
+        }
+    }
+
+    @Test
+    fun splitIterator() {
+    }
+}
