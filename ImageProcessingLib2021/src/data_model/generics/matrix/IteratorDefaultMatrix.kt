1e06d5bbc8333d14993a159dfb85a2ffc54922ac
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-04 23:12:19 +0300

    IteratorDefaultMatrix.kt: improved

--- ImageProcessingLib2021/src/data_model/generics/matrix/IteratorDefaultMatrix.kt
+++ ImageProcessingLib2021/src/data_model/generics/matrix/IteratorDefaultMatrix.kt
@@ -3,21 +3,24 @@ package data_model.generics.matrix
 import data_model.types.Size
 
 class IteratorDefaultMatrix<T : Any>(
-        source: Matrix<T>,
+        val source: Matrix<T>,
         widthOffset: Int,
         heightOffset: Int,
         iteratorSize: Size,
         val defaultValue: T
 ) : IteratorMatrix<T>(source, widthOffset, heightOffset, iteratorSize) {
+
     override fun get(i: Int, j: Int): T {
-        if (i + widthOffset >= width || j + heightOffset >= height)
-            return defaultValue
-        return super.get(i, j)
+        return if (isSafe(i, j)) super.get(i, j) else defaultValue
     }
 
     override fun set(i: Int, j: Int, value: T) {
-        if (i + widthOffset >= width || j + heightOffset >= height)
-            return
-        super.set(i, j, value)
+        if (isSafe(i, j)) super.set(i, j, value)
+    }
+
+    private fun isSafe(i: Int, j: Int): Boolean {
+        return i >= 0 && j >= 0 &&
+                i < iteratorSize.width && j < iteratorSize.height &&
+                i + widthOffset < source.width && j + heightOffset < source.height
     }
 }
