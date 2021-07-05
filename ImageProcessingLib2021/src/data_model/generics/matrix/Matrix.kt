fc021b43a69f6c4e103e06d95d1f5a793108707a
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-05 12:20:33 +0300

    Matrix.kt: equals improved

--- ImageProcessingLib2021/src/data_model/generics/matrix/Matrix.kt
+++ ImageProcessingLib2021/src/data_model/generics/matrix/Matrix.kt
@@ -35,14 +35,13 @@ open class Matrix<T : Any>(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
 
-        other as Matrix<T>
-
-        for (i in 0 until width)
-            for (j in 0 until height)
-                if (matrix[i][j] != other[i, j])
-                    return false
+        (other as? Matrix<T>)?.let {
+            for (i in 0 until width)
+                for (j in 0 until height)
+                    if (matrix[i][j] != other[i, j])
+                        return false
+        } ?: return false
 
         return true
     }
