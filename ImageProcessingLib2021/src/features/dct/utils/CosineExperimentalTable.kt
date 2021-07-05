7287c2e83a5104444e1c9cd3d8fcf8366c661bb8
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-05 22:39:08 +0300

    create CosineExperimentalTable.kt

---
+++ ImageProcessingLib2021/src/features/dct/utils/CosineExperimentalTable.kt
@@ -0,0 +1,42 @@
+package features.dct.utils
+
+@UseExperimental
+class CosineExperimentalTable(
+        val bsf: Int = 8
+) {
+    /**
+     * "Генерирование матрицы ДКП."
+     */
+    fun buildMatDCT() {
+        val dct = Array(bsf) { FloatArray(bsf) }
+        val dctT = Array(bsf) { FloatArray(bsf) }
+        for (x in 0 until bsf) {
+            for (y in 0 until bsf) {
+                val cc = Math.sqrt((2 / bsf).toDouble()) * Math.cos((2 * x + 1) * y * Math.PI / (2 * bsf))
+                dct[y][x] = cc.toFloat()
+            }
+        }
+        for (y in 0 until bsf) {
+            val cc = 1 / Math.sqrt(bsf.toDouble())
+            dct[0][y] = cc.toFloat()
+        }
+        val dctTC = trans(dct)
+        for (x in 0 until bsf) {
+            for (y in 0 until bsf) {
+                dctT[x][y] = dctTC[x][y]
+            }
+        }
+    }
+
+    fun trans(a: Array<FloatArray>): Array<FloatArray> {
+        val ret = Array(a.size) { FloatArray(a[0].size) }
+        for (i in a.indices) {
+            for (j in i until a[i].size) {
+                val temp = a[i][j]
+                ret[i][j] = a[j][i]
+                ret[j][i] = temp
+            }
+        }
+        return ret
+    }
+}
