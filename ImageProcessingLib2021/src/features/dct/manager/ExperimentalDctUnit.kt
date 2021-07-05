7a0eda4a3993cdc87c0a17dc0d0a9d02f5d17c45
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-05 23:17:04 +0300

    create ExperimentalDctUnit.kt

---
+++ ImageProcessingLib2021/src/features/dct/manager/ExperimentalDctUnit.kt
@@ -0,0 +1,89 @@
+package features.dct.manager
+
+import data_model.types.Size
+
+class ExperimentalDctUnit(
+        val imageSize: Size = Size(100, 100),
+        val bs: Int = 8,
+        val bsf: Int = 8,
+        val dct: Array<FloatArray>,
+        val dctT: Array<FloatArray>,
+        val mas0Y: Array<FloatArray>
+) {
+    val xs = imageSize.width
+    val ys = imageSize.height
+    val fxs = rhc(xs, bs)
+    val fys = rhc(ys, bs)
+
+    /**
+     * ДКП яркостной составляющей.
+     */
+    fun YtoDCT() {
+        val mas1Y = Array(fxs / bsf) { Array(fys / bsf) { Array(bsf) { FloatArray(bsf) } } }
+        val mas = Array(fxs / bsf) { Array(fys / bsf) { Array(bsf) { FloatArray(bsf) } } }
+        for (x in 0 until fxs / bsf) {
+            for (y in 0 until fys / bsf) {
+                mas[x][y] = getMasPrt(mas0Y, x, y, bsf)
+            }
+        }
+        for (x in 0 until fxs / bsf) {
+            for (y in 0 until fys / bsf) {
+                mas1Y[x][y] = DCTtoF(mas[x][y])
+            }
+        }
+    }
+
+    fun rhc(s: Int, bs: Int): Int {
+        return if (s % bs == 0) {
+            s
+        } else {
+            ((s / bs) + 1) * bs
+        }
+    }
+
+    fun getMasPrt(mas: Array<FloatArray>, myX: Int, myY: Int, myBs: Int): Array<FloatArray> {
+        val ret = Array(myBs) { FloatArray(myBs) }
+        var x = myX * myBs
+        var xx = 0
+        while (xx < myBs) {
+            var y = myY * myBs
+            var yy = 0
+            while (yy < myBs) {
+                ret[xx][yy] = mas[x][y]
+                y++
+                yy++
+            }
+            x++
+            xx++
+        }
+        return ret
+    }
+
+    fun DCTtoF(`in`: Array<FloatArray>): Array<FloatArray> {
+        for (x in `in`.indices) {
+            for (y in 0 until `in`[x].size) {
+                `in`[x][y] = `in`[x][y] - 128
+            }
+        }
+        var ret = Array(`in`.size) { FloatArray(`in`[0].size) }
+        val in_dct: Array<FloatArray> = mulMat(dct, `in`)
+        val in_dct_dctT: Array<FloatArray> = mulMat(in_dct, dctT)
+        ret = in_dct_dctT
+        return ret
+    }
+
+    fun mulMat(mat0: Array<FloatArray>, mat1: Array<FloatArray>): Array<FloatArray> {
+        val ret = Array(mat0.size) { FloatArray(mat1.size) }
+        val m = mat0.size
+        val n: Int = mat1[0].size
+        val o = mat1.size
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                for (k in 0 until o) {
+                    ret[i][j] += mat0[i][k] * mat1[k][j]
+                }
+            }
+        }
+        return ret
+    }
+}
