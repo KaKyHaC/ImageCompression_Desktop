c3077214f5f1621f762919830cab237d7b5875ef
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-06 19:25:49 +0300

    ModuleOpc2.kt: use Data2

--- ImageProcessingLib2021/src/data_model/processing_data/ProcessingData.kt
+++ ImageProcessingLib2021/src/data_model/processing_data/ProcessingData.kt
@@ -4,6 +4,7 @@ import data_model.generics.Triple
 import data_model.generics.matrix.Matrix
 import data_model.types.ByteVector
 import data_model.types.DataOpc
+import data_model.types.DataOpc2
 
 sealed class ProcessingData {
 
@@ -12,6 +13,8 @@ sealed class ProcessingData {
     data class Image(val triple: Triple<Matrix<Short>>) : ProcessingData()
 
     data class OPC(val triple: Triple<Matrix<out DataOpc>>) : ProcessingData()
+
+    data class OPC2(val triple: Triple<Matrix<out DataOpc2>>) : ProcessingData()
 
     data class Bytes(val byteVector: ByteVector) : ProcessingData()
 }
