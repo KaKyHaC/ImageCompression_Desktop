3745a3a268bafa0f91424703da998531d5bc54a7
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-06 11:15:49 +0300

    DctUtils.kt: updated

--- ImageProcessingLib2021/src/features/dct/utils/DctUtils.kt
+++ ImageProcessingLib2021/src/features/dct/utils/DctUtils.kt
@@ -7,21 +7,20 @@ object DctUtils {
     /**
      * subtract the [0][0] element from each [%8][%8]
      */
-    private fun preProcess(origin: Matrix<Matrix<Short>>) {
-        origin.applyEach { i, j, value ->
-            if (i != 0 && j != 0) {
-                value[0, 0] = (origin[0, 0][0, 0] - value[0, 0]).toShort()
+    private fun preProcess(origin: Matrix<Matrix<Short>>) =
+            origin.applyEach { i, j, value ->
+                if (i != 0 && j != 0) {
+                    value[0, 0] = (origin[0, 0][0, 0] - value[0, 0]).toShort()
+                }
+                null
             }
-            null
-        }
-    }
 
-    fun minus128(arr: Matrix<Short>) {
-        arr.applyEach { i, j, value -> (value - 128).toShort() }
-    }
 
-    fun plus128(arr: Matrix<Short>) {
-        arr.applyEach { i, j, value -> (value + 128).toShort() }
-    }
+    fun minus128(arr: Matrix<Short>) =
+            arr.applyEach { i, j, value -> (value - 128).toShort() }
+
+
+    fun plus128(arr: Matrix<Short>) =
+            arr.applyEach { i, j, value -> (value + 128).toShort() }
 
 }
