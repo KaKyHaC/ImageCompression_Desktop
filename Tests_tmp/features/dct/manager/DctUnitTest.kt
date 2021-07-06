bc569de6a354103b7b8446206b6dca249d526b9e
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-06 14:43:35 +0300

    create DctUnitTest.kt

---
+++ Tests_tmp/features/dct/manager/DctUnitTest.kt
@@ -0,0 +1,35 @@
+package features.dct.manager
+
+import data_model.generics.matrix.Matrix
+import data_model.types.Size
+import org.junit.Test
+import utils.MatrixUtilsTest
+import java.util.*
+
+
+internal class DctUnitTest {
+    val rand = Random()
+
+    @Test
+    fun testHdR0() {
+        test(DctUnit.Parameters(), Size(1920,1080), 0..0)
+    }
+
+    @Test
+    fun testHdR2() {
+        test(DctUnit.Parameters(), Size(1920,1080), 0..2)
+    }
+
+    @Test
+    fun testHdR5() {
+        test(DctUnit.Parameters(), Size(1920,1080), 0..5)
+    }
+
+    fun test(parameters: DctUnit.Parameters, imageSize: Size, range: IntRange) {
+        val origin = Matrix.create(imageSize) { i, j -> rand.nextInt(255).toShort() }
+        val dctUnit = DctUnit(parameters)
+        val direct = dctUnit.direct(origin)
+        val reverse = dctUnit.reverse(direct)
+        MatrixUtilsTest.assertMatrixInRange(origin, reverse, range)
+    }
+}
