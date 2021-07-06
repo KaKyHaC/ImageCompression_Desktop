8b404c6f9982edeeb87acd4c9dca6a3761c5c41a
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-06 19:23:45 +0300

    OpcProcessingManager2.kt: use Data2

--- ImageProcessingLib2021/src/features/opc2/managers/OpcProcessingManager2.kt
+++ ImageProcessingLib2021/src/features/opc2/managers/OpcProcessingManager2.kt
@@ -2,6 +2,7 @@ package features.opc2.managers
 
 import data_model.generics.matrix.Matrix
 import data_model.types.DataOpc
+import data_model.types.DataOpc2
 import data_model.types.Size
 import utils.MatrixUtils
 
@@ -15,12 +16,12 @@ class OpcProcessingManager2(
 
     val unit = OpcProcessUnit2(parameters.params)
 
-    fun direct(image: Matrix<Short>): Matrix<out DataOpc> {
+    fun direct(image: Matrix<Short>): Matrix<out DataOpc2> {
         val splitIterator = MatrixUtils.splitIterator(image, parameters.childSize, 0)
         return splitIterator.map { i, j, value -> unit.direct(value) }
     }
 
-    fun reverse(dataOpcMatrix: Matrix<out DataOpc>, imageSize: Size? = null): Matrix<Short> {
+    fun reverse(dataOpcMatrix: Matrix<out DataOpc2>, imageSize: Size? = null): Matrix<Short> {
         val map = dataOpcMatrix.map { i, j, dataOpc -> unit.reverse(dataOpc, parameters.childSize) }
         return MatrixUtils.gatherMatrix(map, imageSize)
     }
