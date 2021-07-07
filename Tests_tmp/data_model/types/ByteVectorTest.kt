bce15de827742dedbe5210a0c865a50eb8a99673
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-08 00:39:43 +0300

    ByteVectorTest.kt: testMatrix

--- Tests_tmp/data_model/types/ByteVectorTest.kt
+++ Tests_tmp/data_model/types/ByteVectorTest.kt
@@ -1,10 +1,15 @@
 package data_model.types
 
+import data_model.generics.matrix.Matrix
 import org.junit.Test
+import java.nio.ByteBuffer
+import java.util.*
 import kotlin.test.assertEquals
 
 
 internal class ByteVectorTest {
+
+    val rand = Random()
 
     @Test
     fun customTest() {
@@ -30,5 +35,19 @@ internal class ByteVectorTest {
         val reader = byteVector.getReader()
         assertEquals(reader.getInt(), 4)
         assertEquals(reader.getInt(), 123)
+    }
+
+    @Test
+    fun test100(){
+        testMatrix(Size(100))
+    }
+
+    fun testMatrix(size: Size) {
+        val create = Matrix.create(size) { i, j -> rand.nextInt() }
+        val byteVector = ByteVector()
+        create.applyEach{i, j, value -> byteVector.putInt(value); null }
+        val wrap = byteVector.getReader()
+        val create1 = Matrix.create(size) { i, j -> wrap.getInt() }
+        assertEquals(create,create1)
     }
 }
