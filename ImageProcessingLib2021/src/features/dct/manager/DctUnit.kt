120dba4aaa20817ce874b5ccef1edcbd0e176fbb
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 19:45:18 +0300

    DctUnit.kt: improved

--- ImageProcessingLib2021/src/features/dct/manager/DctUnit.kt
+++ ImageProcessingLib2021/src/features/dct/manager/DctUnit.kt
@@ -3,22 +3,35 @@ package features.dct.manager
 import data_model.generics.matrix.Matrix
 import data_model.types.Size
 import features.dct.algorithms.DctAlgorithmExperimental
+import features.dct.algorithms.DctAlgorithmUniversal
 import features.dct.utils.CosineTableFactory
 
 class DctUnit(val parameters: Parameters) {
 
     data class Parameters(
-            val childSize: Size = Size(8, 8)
+            val childSize: Size = Size(8, 8),
+            val algorithmType: AlgorithmType = AlgorithmType.UNIVERSAL
     )
 
-    val experimentalTable = CosineTableFactory.getExperimentalTable(parameters.childSize.width)
-    val algorithmExperimental = DctAlgorithmExperimental(experimentalTable.dct, experimentalTable.dctT)
+    enum class AlgorithmType { EXPERIMENTAL, UNIVERSAL }
+
+    val algorithmExperimental = CosineTableFactory.getExperimentalTable(parameters.childSize.width)
+            .let { DctAlgorithmExperimental(it.dct, it.dctT) }
+
+    val dctAlgorithmUniversal = DctAlgorithmUniversal(parameters.childSize)
+
 
     fun direct(origin: Matrix<Short>): Matrix<Short> {
-        return algorithmExperimental.direct(origin, Double::toShort)
+        return when(parameters.algorithmType) {
+            AlgorithmType.EXPERIMENTAL -> algorithmExperimental.direct(origin, Double::toShort)
+            AlgorithmType.UNIVERSAL -> dctAlgorithmUniversal.direct(origin)
+        }
     }
 
     fun reverse(matrix: Matrix<Short>): Matrix<Short> {
-        return algorithmExperimental.reverse(matrix, Double::toShort)
+        return when(parameters.algorithmType) {
+            AlgorithmType.EXPERIMENTAL -> algorithmExperimental.reverse(matrix, Double::toShort)
+            AlgorithmType.UNIVERSAL -> dctAlgorithmUniversal.reverse(matrix)
+        }
     }
 }
