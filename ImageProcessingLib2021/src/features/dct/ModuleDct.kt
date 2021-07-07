d2b7dcaca619d846cd7b824857369ea2c0be5bc4
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 19:47:24 +0300

    ModuleDct.kt: updated

--- ImageProcessingLib2021/src/features/dct/ModuleDct.kt
+++ ImageProcessingLib2021/src/features/dct/ModuleDct.kt
@@ -3,31 +3,30 @@ package features.dct
 import data_model.generics.Triple
 import data_model.processing_data.ProcessingData
 import features.AbsDataProcessor
-import features.dct.manager.DctUnit
-import features.enlargement.utils.EnlargementUtils
+import features.dct.manager.DctManager
 
 class ModuleDct(
-        val parameters: Parameters
+        val parameters: Parameters = Parameters()
 ) : AbsDataProcessor<ProcessingData.Image, ProcessingData.Image>(
         ProcessingData.Image::class, ProcessingData.Image::class
 ) {
 
     data class Parameters(
-            val processFirst: DctUnit.Parameters? = DctUnit.Parameters(),
-            val processSecond: DctUnit.Parameters? = DctUnit.Parameters(),
-            val processThird: DctUnit.Parameters? = DctUnit.Parameters()
+            val processFirst: DctManager.Parameters? = DctManager.Parameters(),
+            val processSecond: DctManager.Parameters? = DctManager.Parameters(),
+            val processThird: DctManager.Parameters? = DctManager.Parameters()
     )
 
-    private val dctUnit1 = parameters.processFirst?.let { DctUnit(it) }
-    private val dctUnit2 = parameters.processSecond?.let { DctUnit(it) }
-    private val dctUnit3 = parameters.processThird?.let { DctUnit(it) }
+    private val manager1 = parameters.processFirst?.let { DctManager(it) }
+    private val manager2 = parameters.processSecond?.let { DctManager(it) }
+    private val manager3 = parameters.processThird?.let { DctManager(it) }
 
     override fun processDirectTyped(data: ProcessingData.Image): ProcessingData.Image {
         return ProcessingData.Image(
                 Triple(
-                        dctUnit1?.direct(data.triple.first) ?: data.triple.first,
-                        dctUnit2?.direct(data.triple.second) ?: data.triple.second,
-                        dctUnit3?.direct(data.triple.third) ?: data.triple.third
+                        manager1?.direct(data.triple.first) ?: data.triple.first,
+                        manager2?.direct(data.triple.second) ?: data.triple.second,
+                        manager3?.direct(data.triple.third) ?: data.triple.third
                 )
         )
     }
@@ -35,9 +34,9 @@ class ModuleDct(
     override fun processReverseTyped(data: ProcessingData.Image): ProcessingData.Image {
         return ProcessingData.Image(
                 Triple(
-                        dctUnit1?.reverse(data.triple.first) ?: data.triple.first,
-                        dctUnit2?.reverse(data.triple.second) ?: data.triple.second,
-                        dctUnit3?.reverse(data.triple.third) ?: data.triple.third
+                        manager1?.reverse(data.triple.first) ?: data.triple.first,
+                        manager2?.reverse(data.triple.second) ?: data.triple.second,
+                        manager3?.reverse(data.triple.third) ?: data.triple.third
                 )
         )
     }
