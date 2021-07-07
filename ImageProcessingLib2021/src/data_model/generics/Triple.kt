c7e01b5eeb48295d60a22358f9a5778ac1677ac8
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 18:13:42 +0300

    Triple.kt: map

--- ImageProcessingLib2021/src/data_model/generics/Triple.kt
+++ ImageProcessingLib2021/src/data_model/generics/Triple.kt
@@ -4,4 +4,11 @@ data class Triple<T>(
         val first: T,
         val second: T,
         val third: T
-)
+) {
+
+    inline fun <reified R> map(mapper: (T) -> R) = Triple(
+            mapper(first),
+            mapper(second),
+            mapper(third)
+    )
+}
