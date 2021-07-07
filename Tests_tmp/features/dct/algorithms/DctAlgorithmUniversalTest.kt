81df8bc684070d1ddbdff354464cdd18ac5872d1
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 19:37:29 +0300

    create DctAlgorithmUniversalTest.kt

--- Tests_tmp/features/dct/algorithms/DctAlgorithmUniversalTest.kt
+++ Tests_tmp/features/dct/algorithms/DctAlgorithmUniversalTest.kt
@@ -21,11 +21,25 @@ internal class DctAlgorithmUniversalTest {
         test(Size(8,8))
     }
 
+    @Test
+    fun test16() {
+        test(Size(16,16))
+    }
+
+    @Test
+    fun test4() {
+        test(Size(4,4))
+    }
+
     fun test(size:Size) {
         val origin = Matrix.create(size) { i, j -> rand.nextInt(500).toShort() }
+        println("origin = $origin")
         val dctAlgorithmUniversal = DctAlgorithmUniversal(size)
         val direct = dctAlgorithmUniversal.direct(origin)
+        println("direct = $direct")
         val reverse = dctAlgorithmUniversal.reverse(direct)
+        println("reverse = $reverse")
+
         MatrixUtilsTest.assertMatrixInRange(origin, reverse, 0..2)
     }
 }
