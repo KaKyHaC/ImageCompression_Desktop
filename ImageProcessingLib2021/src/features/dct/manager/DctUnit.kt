4dbf13a7229b14db94e094f5c38524e5e0ff6f2a
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 18:52:36 +0300

    DctUnit.kt: updated

--- ImageProcessingLib2021/src/features/dct/manager/DctUnit.kt
+++ ImageProcessingLib2021/src/features/dct/manager/DctUnit.kt
@@ -10,41 +10,17 @@ import utils.MatrixUtils
 class DctUnit(val parameters: Parameters) {
 
     data class Parameters(
-            val childSize: Size = Size(8, 8),
-            val useExperimental: Boolean = true,
-            val subtractFirstElements: Boolean = true,
-            val remove128: Boolean = true
+            val childSize: Size = Size(8, 8)
     )
 
     val experimentalTable = CosineTableFactory.getExperimentalTable(parameters.childSize.width)
-//    val table = CosineTableFactory.getTable(parameters.childSize.width)
-
     val algorithmExperimental = ExperimentalDctAlgorithm(experimentalTable.dct, experimentalTable.dctT)
 
     fun direct(origin: Matrix<Short>): Matrix<Short> {
-        val minus128 = if (parameters.remove128)  DctUtils.minus128(origin) else origin
-        val splitIterator = MatrixUtils.splitIterator(minus128, parameters.childSize, 0)
-        if (parameters.subtractFirstElements) DctUtils.subtractFirstElements(splitIterator)
-        splitIterator.applyEach { i, j, value ->
-            if (parameters.useExperimental)
-                algorithmExperimental.direct(value, Double::toShort)
-            else
-                TODO()
-        }
-        return MatrixUtils.gatherMatrix(splitIterator)
+        return algorithmExperimental.direct(origin, Double::toShort)
     }
 
     fun reverse(matrix: Matrix<Short>): Matrix<Short> {
-        val splitIterator = MatrixUtils.splitIterator(matrix, parameters.childSize, 0)
-        if (parameters.subtractFirstElements) DctUtils.subtractFirstElements(splitIterator)
-        splitIterator.applyEach { i, j, value ->
-            if (parameters.useExperimental)
-                algorithmExperimental.reverse(value, Double::toShort)
-            else
-                TODO()
-        }
-        val gatherMatrix = MatrixUtils.gatherMatrix(splitIterator)
-        val res = if (parameters.remove128)  DctUtils.plus128(gatherMatrix) else gatherMatrix
-        return res
+        return algorithmExperimental.reverse(matrix, Double::toShort)
     }
 }
