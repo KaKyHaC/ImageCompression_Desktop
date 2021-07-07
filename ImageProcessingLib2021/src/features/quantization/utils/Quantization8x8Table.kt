3baeff5126f36163828f77381860e349b73b8975
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 17:37:09 +0300

    Quantization8x8Table.kt: use matrix

--- ImageProcessingLib2021/src/features/quantization/utils/Quantization8x8Table.kt
+++ ImageProcessingLib2021/src/features/quantization/utils/Quantization8x8Table.kt
@@ -8,15 +8,16 @@ import features.quantization.utils.Quantization8x8Table
  * Created by Димка on 08.08.2016.
  */
 object Quantization8x8Table {
-    private val luminosity = arrayOf(
-            shortArrayOf(1, 2, 3, 4, 5, 6, 7, 8),
-            shortArrayOf(2, 2, 3, 4, 5, 8, 10, 15),
-            shortArrayOf(3, 4, 5, 6, 7, 9, 16, 20),
-            shortArrayOf(4, 6, 8, 19, 21, 27, 28, 36),
-            shortArrayOf(5, 12, 27, 36, 48, 59, 69, 77),
-            shortArrayOf(7, 35, 35, 44, 51, 69, 79, 82),
-            shortArrayOf(9, 44, 47, 48, 49, 59, 69, 79),
-            shortArrayOf(12, 49, 55, 68, 79, 80, 99, 99))
+    private val luminosity = Matrix(arrayOf(
+            shortArrayOf(1, 2, 3, 4, 5, 6, 7, 8).toTypedArray(),
+            shortArrayOf(2, 2, 3, 4, 5, 8, 10, 15).toTypedArray(),
+            shortArrayOf(3, 4, 5, 6, 7, 9, 16, 20).toTypedArray(),
+            shortArrayOf(4, 6, 8, 19, 21, 27, 28, 36).toTypedArray(),
+            shortArrayOf(5, 12, 27, 36, 48, 59, 69, 77).toTypedArray(),
+            shortArrayOf(7, 35, 35, 44, 51, 69, 79, 82).toTypedArray(),
+            shortArrayOf(9, 44, 47, 48, 49, 59, 69, 79).toTypedArray(),
+            shortArrayOf(12, 49, 55, 68, 79, 80, 99, 99).toTypedArray()
+    ), Short::class)
 
     /*        luminosity = new short[][]{
                     {1,1,1,2,3,40,51,61},
@@ -28,18 +29,18 @@ object Quantization8x8Table {
                     {49,64,78,87,99,99,99,99},
                     {72,92,95,98,99,90,99,99}};*/
 
-    private val chromaticity = arrayOf(
-            shortArrayOf(1, 1, 2, 4, 9, 99, 99, 99),
-            shortArrayOf(1, 2, 2, 6, 99, 99, 99, 99),
-            shortArrayOf(2, 2, 56, 99, 99, 99, 99, 99),
-            shortArrayOf(4, 66, 99, 99, 99, 99, 99, 99),
-            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99),
-            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99),
-            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99),
-            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99)
-    )
+    private val chromaticity = Matrix(arrayOf(
+            shortArrayOf(1, 1, 2, 4, 9, 99, 99, 99).toTypedArray(),
+            shortArrayOf(1, 2, 2, 6, 99, 99, 99, 99).toTypedArray(),
+            shortArrayOf(2, 2, 56, 99, 99, 99, 99, 99).toTypedArray(),
+            shortArrayOf(4, 66, 99, 99, 99, 99, 99, 99).toTypedArray(),
+            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99).toTypedArray(),
+            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99).toTypedArray(),
+            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99).toTypedArray(),
+            shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99).toTypedArray()
+    ), Short::class)
 
-    fun getLuminosity(x: Int, y: Int) = luminosity[x][y]
+    fun getLuminosity(x: Int, y: Int) = luminosity[x,y]
 
-    fun getChromaticity(x: Int, y: Int) = chromaticity[x][y]
+    fun getChromaticity(x: Int, y: Int) = chromaticity[x,y]
 }
