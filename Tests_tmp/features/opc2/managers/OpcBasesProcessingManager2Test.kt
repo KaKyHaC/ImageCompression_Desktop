5dbae66c8a927c0bf7f84c241db9bc39d226da83
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-07 13:06:17 +0300

    OpcBasesProcessingManager2.kt: testMaxBase

---
+++ Tests_tmp/features/opc2/managers/OpcBasesProcessingManager2Test.kt
@@ -0,0 +1,36 @@
+package features.opc2.managers
+
+import data_model.generics.matrix.Matrix
+import data_model.types.DataOpc2
+import data_model.types.Size
+import org.junit.Test
+import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+
+internal class OpcBasesProcessingManager2Test {
+
+    val rand = Random()
+    val manager = OpcBasesProcessingManager2()
+
+    @Test
+    fun test1() {
+        testMaxBase(Size(10,10), 8)
+    }
+
+    fun testMaxBase(matrixSize: Size, baseSize: Int) {
+        val matrix = Matrix.create(matrixSize) { i, j ->
+            DataOpc2.Base.Max(
+                    ShortArray(baseSize) { rand.nextInt(255).absoluteValue.toShort() }
+            ) as DataOpc2.Base
+        }
+        val direct = manager.direct(matrix)
+        val reverse = manager.reverse(direct, Size(baseSize, 1))
+
+        assertEquals(matrix, reverse)
+        matrix[0,0].baseMax[0]++
+        assertNotEquals(matrix, reverse)
+    }
+}
