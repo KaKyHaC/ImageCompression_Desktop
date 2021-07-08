b8a838302b57b63599a6e2f03cfa0ea181d66d02
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-08 17:26:16 +0300

    implemented ModuleOpcToBytes.kt

--- ImageProcessingLib2021/src/features/opc_format/ModuleOpcToBytes.kt
+++ ImageProcessingLib2021/src/features/opc_format/ModuleOpcToBytes.kt
@@ -1,17 +1,30 @@
 package features.opc_format
 
 import data_model.processing_data.ProcessingData
+import data_model.types.ByteVector
 import features.AbsDataProcessor
+import features.opc_format.manager.OpcBasesToBytesManager
+import features.opc_format.manager.OpcToBytesManager
 
-class ModuleOpcToBytes : AbsDataProcessor<ProcessingData.Opc, ProcessingData.Bytes>(
-        ProcessingData.Opc::class, ProcessingData.Bytes::class
+class ModuleOpcToBytes : AbsDataProcessor<ProcessingData.Opc2, ProcessingData.Bytes>(
+        ProcessingData.Opc2::class, ProcessingData.Bytes::class
 ) {
+    //todo add parameters
 
-    override fun processDirectTyped(data: ProcessingData.Opc): ProcessingData.Bytes {
-        TODO("Not yet implemented")
+    private val baseManager = OpcBasesToBytesManager()
+    private val opcManager = OpcToBytesManager()
+
+    override fun processDirectTyped(data: ProcessingData.Opc2): ProcessingData.Bytes {
+        val byteVector = ByteVector()
+        baseManager.direct(byteVector, data.triple.map { it.map { i, j, dataOpc -> dataOpc.base } }) //todo remove map
+        opcManager.direct(byteVector, data.triple)
+        return ProcessingData.Bytes(byteVector)
     }
 
-    override fun processReverseTyped(data: ProcessingData.Bytes): ProcessingData.Opc {
-        TODO("Not yet implemented")
+    override fun processReverseTyped(data: ProcessingData.Bytes): ProcessingData.Opc2 {
+        val reader = data.byteVector.getReader()
+        val bases = baseManager.reverse(reader)
+        val opcs = opcManager.reverse(reader, bases)
+        return ProcessingData.Opc2(opcs)
     }
 }
