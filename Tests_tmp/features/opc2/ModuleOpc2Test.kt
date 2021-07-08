b5639ba0c076c4f9fc7f63bb5b81e6a2e523f20d
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-08 23:04:23 +0300

    create ModuleOpc2Test.kt

---
+++ Tests_tmp/features/opc2/ModuleOpc2Test.kt
@@ -0,0 +1,39 @@
+package features.opc2
+
+import data_model.generics.Triple
+import data_model.generics.matrix.Matrix
+import data_model.processing_data.ProcessingData
+import data_model.types.Size
+import org.junit.Test
+import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.test.assertEquals
+
+
+internal class ModuleOpc2Test {
+
+    val rand = Random()
+
+    @Test
+    fun test8() {
+        test(Size(8))
+    }
+
+    @Test
+    fun test100() {
+        test(Size(100))
+    }
+
+    fun test(matrixSize: Size) {
+        val moduleOpc2 = ModuleOpc2()
+        val data = ProcessingData.Image(Triple(
+                Matrix.create(matrixSize) { i, j -> rand.nextInt(255).absoluteValue.toShort() },
+                Matrix.create(matrixSize) { i, j -> rand.nextInt(255).absoluteValue.toShort() },
+                Matrix.create(matrixSize) { i, j -> rand.nextInt(255).absoluteValue.toShort() }
+        ))
+        val direct = moduleOpc2.processDirectTyped(data)
+        val processReverseTyped = moduleOpc2.processReverseTyped(direct)
+        assertEquals(data, processReverseTyped)
+    }
+
+}
