390bb3c2765111629cc4b1f626fefaa1115e00d8
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-08 12:49:09 +0300

    create ByteVectorUtils.kt

---
+++ ImageProcessingLib2021/src/features/opc_format/utils/ByteVectorUtils.kt
@@ -0,0 +1,43 @@
+package features.opc_format.utils
+
+import data_model.types.ByteVector
+import data_model.types.DataOpc2
+import data_model.types.Size
+import kotlin.experimental.and
+
+object ByteVectorUtils {
+
+    object Bases {
+
+        enum class Type { MAX, MIN_AND_MAX }
+
+        fun direct(byteVector: ByteVector, bases: DataOpc2.Base, type: Type = Type.MAX) {
+            bases.baseMax.forEach { byteVector.putByte(it.toByte()) }
+            if (type == Type.MIN_AND_MAX)
+                (bases as DataOpc2.Base.MaxMin).baseMin.forEach { byteVector.putByte(it.toByte()) }
+        }
+
+        fun reverse(reader: ByteVector.Read, baseSize: Int, type: Type = Type.MAX): DataOpc2.Base {
+            val max = ShortArray(baseSize) { reader.nextByte().toShort() and 0xff }
+            return if (type == Type.MIN_AND_MAX) {
+                val min = ShortArray(baseSize) { reader.nextByte().toShort() and 0xff }
+                DataOpc2.Base.MaxMin(max, min)
+            } else {
+                DataOpc2.Base.Max(max)
+            }
+        }
+
+    }
+
+    object Size {
+        fun direct(byteVector: ByteVector, size: data_model.types.Size) {
+            byteVector.putShort(size.width.toShort())
+            byteVector.putShort(size.height.toShort())
+        }
+
+        fun reverse(reader: ByteVector.Read) = data_model.types.Size(
+                reader.nextShort().toInt(),
+                reader.nextShort().toInt()
+        )
+    }
+}
