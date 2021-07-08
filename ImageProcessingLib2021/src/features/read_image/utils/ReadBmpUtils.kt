a9e4343267432cd4bf4fa3156bb819b70d4b210c
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-09 00:07:35 +0300

    create ReadBmpUtils.kt

---
+++ ImageProcessingLib2021/src/features/read_image/utils/ReadBmpUtils.kt
@@ -0,0 +1,30 @@
+package features.read_image.utils
+
+import java.awt.image.BufferedImage
+import data_model.generics.Triple
+import data_model.generics.matrix.Matrix
+import data_model.types.Size
+
+object ReadBmpUtils {
+
+    const val ROLL_ALFA = 24
+    const val ROLL_RED = 16
+    const val ROLL_GREEN = 8
+    const val ROLL_BLUE = 0
+
+    fun map(bufferedImage: BufferedImage): Triple<Matrix<Short>> {
+        val size = Size(bufferedImage.width, bufferedImage.height)
+        val r = Matrix.create<Short>(size) { i, j -> 0 }
+        val g = Matrix.create<Short>(size) { i, j -> 0 }
+        val b = Matrix.create<Short>(size) { i, j -> 0 }
+        for (i in 0..size.width) {
+            for (j in 0..size.height) {
+                val rgb = bufferedImage.getRGB(i, j)
+                r[i, j] = (rgb shr ROLL_RED and 0xff).toShort()
+                g[i, j] = (rgb shr ROLL_GREEN and 0xff).toShort()
+                b[i, j] = (rgb shr ROLL_BLUE and 0xff).toShort()
+            }
+        }
+        return Triple(r, g, b)
+    }
+}
