3968180b6b31b26acdca3fa2f777cea0b53402cc
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-08 12:51:27 +0300

    OpcBasesToBytesUnit.kt: improved

--- ImageProcessingLib2021/src/features/opc_format/manager/OpcBasesToBytesUnit.kt
+++ ImageProcessingLib2021/src/features/opc_format/manager/OpcBasesToBytesUnit.kt
@@ -4,15 +4,16 @@ import data_model.generics.matrix.Matrix
 import data_model.types.ByteVector
 import data_model.types.DataOpc2
 import data_model.types.Size
+import features.opc_format.utils.ByteVectorUtils
 import features.opc_format.utils.OpcBasesToBytesUtils
 import kotlin.experimental.and
 
-class OpcBasesToBytesManager(
+class OpcBasesToBytesUnit(
         val parameters: Parameters = Parameters()
 ) {
 
     data class Parameters(
-            val type: OpcBasesToBytesUtils.Type = OpcBasesToBytesUtils.Type.MAX
+            val type: ByteVectorUtils.Bases.Type = ByteVectorUtils.Bases.Type.MAX
     )
 
     fun direct(
@@ -20,12 +21,12 @@ class OpcBasesToBytesManager(
             basesMatrix: Matrix<DataOpc2.Base>
     ) {
         basesMatrix.applyEach { i, j, value ->
-            OpcBasesToBytesUtils.direct(byteVector, value, parameters.type)
+            ByteVectorUtils.Bases.direct(byteVector, value, parameters.type)
             null
         }
     }
 
     fun reverse(reader: ByteVector.Read, baseSize: Int, matrixSize: Size): Matrix<DataOpc2.Base> {
-        return Matrix.create(matrixSize) {i, j -> OpcBasesToBytesUtils.reverse(reader, baseSize) }
+        return Matrix.create(matrixSize) { i, j -> ByteVectorUtils.Bases.reverse(reader, baseSize) }
     }
 }
