f4fa94cb797b1bbd489fbdd930cd3caf6947a74f
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-08 14:48:11 +0300

    ByteVectorUtilsTest.kt: improved

--- Tests_tmp/features/opc_format/utils/ByteVectorUtilsTest.kt
+++ Tests_tmp/features/opc_format/utils/ByteVectorUtilsTest.kt
@@ -10,6 +10,7 @@ import java.math.BigInteger
 import java.util.*
 import kotlin.math.absoluteValue
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 
 internal class ByteVectorUtilsTest {
@@ -17,12 +18,17 @@ internal class ByteVectorUtilsTest {
     val rand = Random()
 
     @Test
-    fun testCode8(){
+    fun testCode8() {
         testCode(Size(8))
     }
 
+    @Test
+    fun testCode16() {
+        testCode(Size(16))
+    }
+
     fun testCode(size: Size) {
-        val matrix = Matrix.create(size) { i, j -> rand.nextInt(10000).absoluteValue.toShort() }
+        val matrix = Matrix.create(size) { i, j -> rand.nextInt(2).absoluteValue.toShort() }
         val opcProcessUnit2 = OpcProcessUnit2(OpcProcessUnit2.Parameters())
         val opc = opcProcessUnit2.direct(matrix)
         val byteVector = ByteVector()
@@ -30,6 +36,8 @@ internal class ByteVectorUtilsTest {
         ByteVectorUtils.Code.direct(byteVector, opc.code as DataOpc2.Code.BI, opc.base, size)
         val reverse = ByteVectorUtils.Code.reverse(byteVector.getReader(), opc.base, size)
 
-        assertEquals(opc.code, reverse)
+        assertEquals((opc.code as DataOpc2.Code.BI).N, reverse.N)
+        reverse.N.add(BigInteger.TEN)
+        assertNotEquals((opc.code as DataOpc2.Code.BI).N, reverse.N)
     }
 }
