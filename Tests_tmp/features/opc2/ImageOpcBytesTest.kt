fc7e3c2591422de59bcbbbe4094bade50675df0b
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-09 17:04:01 +0300

    create ImageOpcBytesTest.kt

---
+++ Tests_tmp/features/opc2/ImageOpcBytesTest.kt
@@ -0,0 +1,62 @@
+package features.opc2
+
+import data_model.generics.Triple
+import data_model.generics.matrix.Matrix
+import data_model.processing_data.ProcessingData
+import data_model.types.Size
+import features.opc2.ModuleOpc2
+import features.opc_format.ModuleOpcToBytes
+import org.junit.Test
+import utils.MatrixUtils
+import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+
+internal class ImageOpcBytesTest {
+
+    val rand = Random()
+
+    @Test
+    fun test16() {
+        test(Size(16))
+    }
+
+    @Test
+    fun test1128() {
+        test(Size(128))
+    }
+
+    @Test
+    fun test128() {
+        test(Size(128))
+    }
+
+    fun test(matrixSize: Size) {
+        val moduleOpc2 = ModuleOpc2()
+        val moduleOpcToBytes = ModuleOpcToBytes()
+
+        val imageData = ProcessingData.Image(Triple(
+                Matrix.create(matrixSize) { i, j -> rand.nextInt(255).absoluteValue.toShort() },
+                Matrix.create(Size(8, 8)) { i, j -> rand.nextInt(255).absoluteValue.toShort() },
+                Matrix.create(Size(16, 16)) { i, j -> rand.nextInt(255).absoluteValue.toShort() })
+        )
+
+        val copy = ProcessingData.Image(Triple(
+                MatrixUtils.copy(imageData.triple.first),
+                MatrixUtils.copy(imageData.triple.second),
+                MatrixUtils.copy(imageData.triple.third)
+        ))
+
+        val opcData = moduleOpc2.processDirectTyped(imageData)
+        val bytesData = moduleOpcToBytes.processDirectTyped(opcData)
+
+        val reverseOpc = moduleOpcToBytes.processReverseTyped(bytesData)
+        val reverseImage = moduleOpc2.processReverseTyped(reverseOpc)
+
+        assertEquals(copy, reverseImage)
+        copy.triple.first[0, 0]++
+        assertNotEquals(copy, reverseImage)
+    }
+}
