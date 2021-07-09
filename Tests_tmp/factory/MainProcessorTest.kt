2b9bf11bd35276aed86609ef6d39cb287aa84a19
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-09 12:36:13 +0300

    create MainProcessorTest.kt

---
+++ Tests_tmp/factory/MainProcessorTest.kt
@@ -0,0 +1,16 @@
+package factory
+
+import data_model.processing_data.ProcessingData
+import org.junit.Test
+import java.io.File
+
+
+internal class MainProcessorTest {
+
+    @Test
+    fun testDefault() {
+        val mainProcessor = MainProcessor(MainProcessor.Params(ProcessorModuleFactorySamples.defaultList))
+        mainProcessor.run(ProcessingData.File(File("outTest.bmp")))
+        mainProcessor.run(ProcessingData.File(File("image.bar")), MainProcessor.Mode.REVERSE)
+    }
+}
