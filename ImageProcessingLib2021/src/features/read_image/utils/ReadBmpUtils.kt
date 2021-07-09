3d996f4a8732bc6da9abf76f8c8ceb373cba09aa
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-09 11:53:28 +0300

    create ReadBmpUtils.kt

--- ImageProcessingLib2021/src/features/read_image/utils/ReadBmpUtils.kt
+++ ImageProcessingLib2021/src/features/read_image/utils/ReadBmpUtils.kt
@@ -17,8 +17,8 @@ object ReadBmpUtils {
         val r = Matrix.create<Short>(size) { i, j -> 0 }
         val g = Matrix.create<Short>(size) { i, j -> 0 }
         val b = Matrix.create<Short>(size) { i, j -> 0 }
-        for (i in 0..size.width) {
-            for (j in 0..size.height) {
+        for (i in 0 until size.width) {
+            for (j in 0 until size.height) {
                 val rgb = bufferedImage.getRGB(i, j)
                 r[i, j] = (rgb shr ROLL_RED and 0xff).toShort()
                 g[i, j] = (rgb shr ROLL_GREEN and 0xff).toShort()
@@ -32,11 +32,11 @@ object ReadBmpUtils {
     fun map(bmp: Triple<Matrix<Short>>): BufferedImage {
         val size = bmp.first.size
         val bufferedImage = BufferedImage(size.width, size.height, BufferedImage.TYPE_3BYTE_BGR)
-        for (i in 0..size.width) {
-            for (j in 0..size.height) {
+        for (i in 0 until size.width) {
+            for (j in 0 until size.height) {
                 val r = bmp.first[i, j].toInt() shl ROLL_RED
                 val g = bmp.second[i, j].toInt() shl ROLL_GREEN
-                val b = bmp.second[i, j].toInt() shl ROLL_BLUE
+                val b = bmp.third[i, j].toInt() shl ROLL_BLUE
                 bufferedImage.setRGB(i, j, r and g and b)
             }
         }
