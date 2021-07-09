e99df18d01d3a1fdbba23c209155cc561313dc69
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-09 12:03:17 +0300

    ModuleReadImage.kt: implemented

--- ImageProcessingLib2021/src/features/read_image/ModuleReadImage.kt
+++ ImageProcessingLib2021/src/features/read_image/ModuleReadImage.kt
@@ -2,16 +2,33 @@ package features.read_image
 
 import data_model.processing_data.ProcessingData
 import features.AbsDataProcessor
+import features.read_image.utils.ReadBmpUtils
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
-class ModuleReadImage() : AbsDataProcessor<ProcessingData.File, ProcessingData.Image>(
+class ModuleReadImage(
+        val parameters: Parameters = Parameters()
+) : AbsDataProcessor<ProcessingData.File, ProcessingData.Image>(
         ProcessingData.File::class, ProcessingData.Image::class
 ) {
 
+    data class Parameters(
+            val fileName: String = "image.bmp"
+    )
+
     override fun processDirectTyped(data: ProcessingData.File): ProcessingData.Image {
-        TODO("Not yet implemented")
+        val file = File(parameters.fileName)
+        val bufferedImage = ImageIO.read(file)
+        val map = ReadBmpUtils.map(bufferedImage)
+        return ProcessingData.Image(map)
     }
 
     override fun processReverseTyped(data: ProcessingData.Image): ProcessingData.File {
-        TODO("Not yet implemented")
+        val file = File(parameters.fileName)
+        file.createNewFile()
+        val map = ReadBmpUtils.map(data.triple)
+        ImageIO.write(map, "bmp", file.outputStream())
+        return ProcessingData.File(file)
     }
 }
