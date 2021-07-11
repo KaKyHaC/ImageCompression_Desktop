3e197036a89bfe2e4e3bb5d32f37143607eb9571
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-11 14:41:43 +0300

    create ModuleOpc2God.kt

---
+++ ImageProcessingLib2021/src/features/opc2_god_format/ModuleOpc2God.kt
@@ -0,0 +1,40 @@
+package features.opc2_god_format
+
+import data_model.processing_data.ProcessingData
+import data_model.types.ByteVector
+import features.AbsDataProcessor
+import features.opc2.ModuleBasesOpc2
+import features.opc_format.manager.OpcBasesToBytesManager
+import features.opc_format.manager.OpcToBytesManager
+
+class ModuleOpc2God(
+        val parameters: Parameters = Parameters()
+) : AbsDataProcessor<ProcessingData.Opc2, ProcessingData.Bytes>(
+        ProcessingData.Opc2::class, ProcessingData.Bytes::class
+) {
+    data class Parameters(
+            val basesModuleParams: ModuleBasesOpc2.Parameters = ModuleBasesOpc2.Parameters(),
+            val basesParams: OpcBasesToBytesManager.Parameters = OpcBasesToBytesManager.Parameters(),
+            val opcParams: OpcToBytesManager.Parameters = OpcToBytesManager.Parameters()
+    )
+
+    private val moduleBasesOpc2 = ModuleBasesOpc2(parameters.basesModuleParams)
+    private val baseManager = parameters.basesParams.let { OpcBasesToBytesManager(it) }
+    private val opcManager = parameters.opcParams.let { OpcToBytesManager(it) }
+
+
+    override fun processDirectTyped(data: ProcessingData.Opc2): ProcessingData.Bytes {
+        val byteVector = ByteVector()
+        val basesData = ProcessingData.Opc2.Bases(data)
+        val basesOpc = moduleBasesOpc2.processDirectTyped(basesData)
+        val basesOpcBase = basesOpc.triple.map { it.map { i, j, value -> value.base } }
+        baseManager.direct(byteVector, basesOpcBase)
+        opcManager.direct(byteVector, basesOpc.triple)
+        opcManager.direct(byteVector, data.triple)
+        return ProcessingData.Bytes(byteVector)
+    }
+
+    override fun processReverseTyped(data: ProcessingData.Bytes): ProcessingData.Opc2 {
+        TODO("Not yet implemented")
+    }
+}
