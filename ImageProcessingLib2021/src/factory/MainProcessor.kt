53569e7e76769a3201faef47ff9ea305cb084102
Dmitry Barannik <d.v.barannik@gmail.com>
2021-07-19 17:31:51 +0300

    MainProcessor.kt: use TimeCalculatorUtils.kt

--- ImageProcessingLib2021/src/factory/MainProcessor.kt
+++ ImageProcessingLib2021/src/factory/MainProcessor.kt
@@ -1,17 +1,20 @@
 package factory
 
 import data_model.processing_data.ProcessingData
+import utils.TimeCalculatorUtils
 
 class MainProcessor(val params: Params) {
 
     private val modules = params.moduleParams.map { ProcessorModuleFactory.getModule(it) }
 
     fun run(data: ProcessingData, mode: Mode = Mode.DIRECT): ProcessingData {
+        TimeCalculatorUtils.startNewTrack("start $mode: $data")
         var tmp = data
         when (mode) {
-            Mode.DIRECT -> modules.forEach { tmp = it.processDirect(tmp) }
-            Mode.REVERSE -> modules.reversed().forEach { tmp = it.processReverse(tmp) }
+            Mode.DIRECT -> modules.forEach { tmp = it.processDirect(tmp); TimeCalculatorUtils.append(it.toString()) }
+            Mode.REVERSE -> modules.reversed().forEach { tmp = it.processReverse(tmp); TimeCalculatorUtils.append(it.toString()) }
         }
+        println("TimeCalculatorUtils = " + TimeCalculatorUtils.getInfoInPercent())
         return tmp
     }
 
